936e606b2bb12517e757ad2f0eb7bc2b3797169c
Sara Cemal <you@example.com>
2020-11-05 22:17:07 -0500

    wip

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,19 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+    def change
+        create_table :costume_stores do |t|
+            t.string :name
+            t.text :location
+            t.integer :costume_inventory
+            t.boolean :still_in_business
+            t.text :opening_time
+            t.text :closing_time
+            t.timestamps
+          end
+        end
+    end
+    
+    
+#opening time --
+#closing time
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,18 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+    def change
+        create_table :haunted_houses do |t|
+            t.string :name
+            t.text :location
+            t.string :theme
+            t.float :price
+            t.boolean :family_friendly
+            t.text :opening_date
+            t.text :closing_date
+            t.text :description
+            t.timestamps
+          end
+        end
+    end
 
-# !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+
